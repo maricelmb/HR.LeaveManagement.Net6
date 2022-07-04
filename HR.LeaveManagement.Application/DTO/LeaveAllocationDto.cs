f00829ed8a84ca9a4fffcd6e72ecb5e0809b2009
maricelmb <maricelmb@gmail.com>
2022-07-04 08:48:12 +0800

    Added dto's

---
+++ HR.LeaveManagement.Application/DTO/LeaveAllocationDto.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTO.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.DTO
+{
+    public class LeaveAllocationDto : BaseDto
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveTypeDto LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+        public string EmployeeId { get; set; }
+    }
+}
---
+++ HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AutoMapper;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Application.DTO;
+
+namespace HR.LeaveManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+
+        }
+    }
+}
