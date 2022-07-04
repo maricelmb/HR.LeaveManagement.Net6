6e00a1e89333b8a8b0c16e8f13e3effce79c060c
maricelmb <maricelmb@gmail.com>
2022-07-05 01:03:48 +0800

    Add leave type handlers and queries

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Handlers/Queries/GetLeaveTypeListRequestHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTO;
+using HR.LeaveManagement.Application.Features.LeaveType.Request;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Handlers.Queries
+{
+    public class GetLeaveTypeListRequestHandler : IRequestHandler<GetLeaveTypeListRequest, List<LeaveTypeDto>>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;   
+        public GetLeaveTypeListRequestHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+
+        public Task<List<LeaveTypeDto>> Handle(GetLeaveTypeListRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
