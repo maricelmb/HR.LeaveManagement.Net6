a762f011961402d8b6c726513f69e1f81bbb3f59
maricel <maricelmb@gmail.com>
2022-07-09 23:10:09 +0800

    Added a generic response that can be used in command handlers.  Modified the handler for createLeaveType to return the BaseCommandResponse

--- HR.LeaveManagement.Application/Features/LeaveTypes/Handlers/Commands/CreateLeaveTypeCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Handlers/Commands/CreateLeaveTypeCommandHandler.cs
@@ -3,12 +3,13 @@ using HR.LeaveManagement.Application.DTO.LeaveType.Validators;
 using HR.LeaveManagement.Application.Exceptions;
 using HR.LeaveManagement.Application.Features.LeaveTypes.Request.Commands;
 using HR.LeaveManagement.Application.Persistence.Contracts;
+using HR.LeaveManagement.Application.Response;
 using HR.LeaveManagement.Domain;
 using MediatR;
 
 namespace HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Commands
 {
-    public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+    public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, BaseCommandResponse>
     {
         private readonly IMapper _mapper;
         private readonly ILeaveTypeRepository _leaveTypeRepository;
@@ -18,18 +19,29 @@ namespace HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Commands
             _leaveTypeRepository = leaveTypeRepository;
         }
 
-        public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+        public async Task<BaseCommandResponse> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
         {
+            var response = new BaseCommandResponse();
+
             var validator = new CreateLeaveTypeDtoValidator();
             var validationResult = await validator.ValidateAsync(request.CreateLeaveTypeDto);
 
             if (validationResult.IsValid == false)
-                throw new ValidationException(validationResult);
+            {
+                response.Success = false;
+                response.Message = "Creation Failed";
+                response.Errors = validationResult.Errors.Select(x => x.ErrorMessage).ToList(); 
+            }
 
             var leaveType = _mapper.Map<LeaveType>(request.CreateLeaveTypeDto);
 
             leaveType = await _leaveTypeRepository.Add(leaveType);
-            return leaveType.Id;    
+
+            response.Success = true;
+            response.Message = "Creation Successful";
+            response.Id = leaveType.Id;
+
+            return response;    
         }
     }
 }
--- HR.LeaveManagement.Application/Features/LeaveTypes/Request/Commands/CreateLeaveTypeCommand.cs
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Request/Commands/CreateLeaveTypeCommand.cs
@@ -1,14 +1,10 @@
 ﻿using HR.LeaveManagement.Application.DTO;
+using HR.LeaveManagement.Application.Response;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.Features.LeaveTypes.Request.Commands
 {
-    public class CreateLeaveTypeCommand : IRequest<int>
+    public class CreateLeaveTypeCommand : IRequest<BaseCommandResponse>
     {
         public CreateLeaveTypeDto CreateLeaveTypeDto { get; set; }
     }
