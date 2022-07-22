1d559cee626c0c43216c2858ecd8caf56d5dde33
maricel <maricelmb@gmail.com>
2022-07-22 23:59:39 +0800

    Added MVC as client

--- HR.LeaveManagement.API/Program.cs
+++ HR.LeaveManagement.API/Program.cs
@@ -31,7 +31,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseMiddleware<ExceptionMiddleware>();
+//app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 
